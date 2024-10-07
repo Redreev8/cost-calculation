64f0089bd8a2d2bfa2451df2da96637f1f8fb846
redreev8 <redreevs@mail.ru>
2024-10-07 16:55:34 +0500

    prettierrc && lint update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,7 @@
-import FlipCard from '@/ui/flip-card'
-import ScrollBar from '@/ui/scroll-bar'
-
 const Home = async () => {
     return (
-        <ScrollBar valueScrool={16}>
-            <>
-                <FlipCard>
-                    <div>Lorem, </div>
-                    <div>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate hic veritatis veniam
-                        maiores blanditiis, sunt, aspernatur, mollitia sapiente sequi autem voluptatum nobis ipsum unde.
-                        Molestias, numquam? Mollitia sit modi provident.
-                    </div>
-                </FlipCard>
-            </>
-        </ScrollBar>
+        <div>
+        </div>
     )
 }
 
